be5c903c769c8a621e7775de840528cdcfa830dc
zlatkoverk <zlatko112@gmail.com>
2020-06-25 20:18:41 +0200

    Dont serialize views on api

--- ObjectStorage/MetaModel/Class.cs
+++ ObjectStorage/MetaModel/Class.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 namespace ObjectStorage.MetaModel
 {
@@ -12,9 +13,11 @@ namespace ObjectStorage.MetaModel
         [Key] public string Name { get; set; }
         public IList<Property> Properties { get; set; } = new List<Property>();
         public Property PresentationProperty { get; set; }
+        [JsonIgnore]
         public string OverviewTemplate { get; set; } = File.ReadAllText(Path.Combine(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
             "Template/Overview.liquid"));
+        [JsonIgnore]
         public string DetailsTemplate { get; set; } = File.ReadAllText(Path.Combine(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
             "Template/Details.liquid"));
